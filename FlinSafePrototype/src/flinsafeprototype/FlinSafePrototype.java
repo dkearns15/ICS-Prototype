c8bbb0a9b30d94ae648e50a4ef26b74b05878e46
dkearns15 <dkearns@hotmail.com.au>
2018-04-04 18:16:57 +0930

    Added MainMenu

--- FlinSafePrototype/src/flinsafeprototype/FlinSafePrototype.java
+++ FlinSafePrototype/src/flinsafeprototype/FlinSafePrototype.java
@@ -15,7 +15,7 @@ public class FlinSafePrototype {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new MainMenu().setVisible(true);
     }
     
 }
