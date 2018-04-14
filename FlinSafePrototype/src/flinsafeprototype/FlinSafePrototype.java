2f7fadc8d5b051c6f86be5f4b789ab194e86c168
dkearns15 <dkearns@hotmail.com.au>
2018-04-14 16:56:11 +0930

    Added JCommon and JFreeChart. Added a sample pie chart on start up.

--- FlinSafePrototype/src/flinsafeprototype/FlinSafePrototype.java
+++ FlinSafePrototype/src/flinsafeprototype/FlinSafePrototype.java
@@ -5,6 +5,11 @@
  */
 package flinsafeprototype;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
 /**
  *
  * @author dkear
@@ -16,6 +21,23 @@ public class FlinSafePrototype {
      */
     public static void main(String[] args) {
         new MainMenu().setVisible(true);
+        // create a dataset...
+        DefaultPieDataset data = new DefaultPieDataset();
+        data.setValue("Category 1", 43.2);
+        data.setValue("Category 2", 27.9);
+        data.setValue("Category 3", 79.5);
+// create a chart..
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Sample Pie Chart",
+                data,
+                true, // legend?
+                true, // tooltips?
+                false // URLs
+        );
+
+        ChartFrame frame = new ChartFrame("First", chart);
+        frame.pack();
+        frame.setVisible(true);
     }
-    
+
 }
