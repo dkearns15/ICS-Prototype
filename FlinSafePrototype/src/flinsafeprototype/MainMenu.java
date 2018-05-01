b50637b21b0953d8e1d791d50c3dfe9a727a8680
John <John@169.254.2.184>
2018-05-02 00:04:43 +0930

    Change main menu to resize child window, add close button to child

--- FlinSafePrototype/src/flinsafeprototype/MainMenu.java
+++ FlinSafePrototype/src/flinsafeprototype/MainMenu.java
@@ -8,6 +8,7 @@ package flinsafeprototype;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 
 /**
  *
@@ -137,7 +138,11 @@ public class MainMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_securitySummaryButtonActionPerformed
 
     private void reportActivityButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_reportActivityButtonActionPerformed
-        new ReportActivityMain().setVisible(true);
+        ReportActivityMain RAframe = new ReportActivityMain();
+        RAframe.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        //RAframe.setUndecorated(true);
+        RAframe.setVisible(true);
+        //new ReportActivityMain().setVisible(true);
     }//GEN-LAST:event_reportActivityButtonActionPerformed
 
     private void kioskButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kioskButtonActionPerformed
